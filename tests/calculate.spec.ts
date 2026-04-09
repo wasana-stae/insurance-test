fd6bf82d15cfeafb7396ad77e61e8956cbcb7a77
Milkkioh <>
2026-04-09 13:12:07 +0700

    delete link thailand

--- tests/calculate.spec.ts
+++ tests/calculate.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+//given go to roojai.com
   await page.goto('https://www.roojai.com/');
   await page.getByRole('button', { name: 'เช็คราคา' }).click();
   await page.getByRole('link', { name: 'Get quote travel insurance' }).click();
-  await page.getByRole('button', { name: 'เดินทางภายในประเทศ เที่ยวไทยแบบอุ่นใจทุกเส้นทาง' }).click();
-  await page.getByRole('button', { name: 'จังหวัดต้นทาง' }).click();
+//when click on "เดินทางต่างประเทศ ท่องเที่ยวออกสู่โลกกว้าง"
   await page.getByRole('button', { name: 'เดินทางต่างประเทศ ท่องเที่ยวออกสู่โลกกว้าง' }).click();
   await page.getByRole('button', { name: 'เลือกจุดหมายปลายทาง' }).click();
+//then select "China"
   await page.getByRole('button', { name: 'China' }).click();
   await page.getByRole('button', { name: 'ปิด' }).click();
   await page.getByRole('button', { name: 'ต่อไป ↓' }).click();
@@ -18,4 +19,6 @@ test('test', async ({ page }) => {
   await page.getByLabel('เมษายน 30,').first().click();
   await page.getByRole('button', { name: 'ต่อไป ↓' }).click();
   await page.getByRole('button', { name: 'ดูราคาของคุณ' }).click();
+// screenshot  await page.screenshot({ path: 'screenshot.png' });
+await page.screenshot({ path: 'test-results/search-report.png', fullPage: true });
 });
